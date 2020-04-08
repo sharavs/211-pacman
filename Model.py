33f97bee486a749ae810db96630f65e4e376b5f3
Sharav Sadhu <sharav.sadhu@outlook.com>
2020-04-07 22:45:24 -0400

    Revert "Merge branch 'master' into nilabh_create_characters_branch"
    This reverts commit bbfbc1e8b889e96c6c9b7085555ed1f2a1c4e2a5, reversing
    changes made to a711c7cb2f263545c4ee8e68d765f4f16db94cda.
    
    # Conflicts:
    #	View.py

--- Model.py
+++ Model.py
@@ -1,48 +1,19 @@
+import csv
 import os
+import View as v
+import csv
 import shelve
 from datetime import datetime
 import pickle
 import os.path
-import time
 
-class TXTModel:
+class CSVModel:
     ''' CSV Storage'''
+    
+    fields = {'ID':None,'Password':None,'Text':None}
+    
+    
+    def __init__(self, saved_data):
+            self.saved_data = saved_data
+            self.load_data = {}
 
-    fields = {'ID': None, 'Password': None, 'Text': None}
-
-    def __init__(self, filename):
-        self.filename = filename
-        self.filecontent = []
-        # self.saved_data = saved_data
-        self.tstring = time.strftime("%Y%m%d-%H%M%S")
-
-    def save_game(self, score, map):
-        newfile = not os.path.exists('game'+self.tstring)  # check if it's a new file
-        if newfile:
-            f = open(('game'+self.tstring+'.txt'), 'w')
-            f.write('[{}]'.format(str(score)) + '\n')
-            f.write(str(map) + '\n')
-        else:
-            f = open((self.filename, 'w'))
-            f.write('[{}]'.format(str(score)) + '\n')
-            f.write(str(map) + '\n')
-            f.close()
-        f.close()
-
-    def load_game(self):
-        f = open(self.filename, 'r')
-        records = []
-
-        x = 0
-        for x in range(2):
-            item = f.readline()
-            self.filecontent.append(item)
-            x += 1
-        for i in self.filecontent:
-            records.append(i[:-1])
-        f.close()
-        return records
-
-    def filer(self):
-        file = self.filename
-        return file
